a8728603af059617e1ec22d77eac74975ebb1ea8
yesak1 <23kkazliner@athenian.org>
2020-10-09 03:38:07 +0000

    save

--- my_code.py
+++ my_code.py
@@ -307,6 +307,244 @@ def jerseynumber():
         elif asklegendint15==2:
             print(list_15[1])
 
+    elif inputjersey==16:
+        list_16=["Juwan Morgan", "Pau Gasol"]
+        asklegendint16=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint16==1:
+            print(list_16[0])
+        elif asklegendint16==2:
+            print(list_16[1])
+
+    elif inputjersey==17:
+        list_17=["Jonas Valanciunas", "Pau Gasol"]
+        asklegendint17=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint17==1:
+            print(list_17[0])
+        elif asklegendint17==2:
+            print(list_17[1])
+
+    elif inputjersey==18:
+        list_18=["Dion Waiters", "Dave Cowens"]
+        asklegendint18=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint18==1:
+            print(list_18[0])
+        elif asklegendint18==2:
+            print(list_18[1])
+
+    elif inputjersey==19:
+        list_19=["Tyson Chandler", "Willis Reed"]
+        asklegendint19=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint19==1:
+            print(list_19[0])
+        elif asklegendint19==2:
+            print(list_19[1])
+
+    elif inputjersey==20:
+        list_20=["Gordon Hayward", "Gary Payton"]
+        asklegendint20=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint20==1:
+            print(list_20[0])
+        elif asklegendint20==2:
+            print(list_20[1])
+
+    elif inputjersey==21:
+        list_21=["Joel Embiid", "Tim Duncan"]
+        asklegendint21=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint21==1:
+            print(list_21[0])
+        elif asklegendint21==2:
+            print(list_21[1])
+
+    elif inputjersey==22:
+        list_22=["Jimmy Butler", "Clyde Drexler"]
+        asklegendint22=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint22==1:
+            print(list_22[0])
+        elif asklegendint22==2:
+            print(list_22[1])
+
+    elif inputjersey==23:
+        list_23=["LeBron James", "Michael Jordan"]
+        asklegendint23=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint23==1:
+            print(list_23[0])
+        elif asklegendint23==2:
+            print(list_23[1])
+
+    elif inputjersey==24:
+        list_24=["Buddy Hield", "Kobe Bryant"]
+        asklegendint24=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint24==1:
+            print(list_24[0])
+        elif asklegendint24==2:
+            print(list_24[1])
+
+    elif inputjersey==25:
+        list_25=["Ben Simmons", "Chet Walker"]
+        asklegendint25=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint25==1:
+            print(list_25[0])
+        elif asklegendint25==2:
+            print(list_25[1])
+
+    elif inputjersey==26:
+        list_26=["Kyle Korver", "James Robinson"]
+        asklegendint26=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint26==1:
+            print(list_26[0])
+        elif asklegendint26==2:
+            print(list_26[1])
+
+    elif inputjersey==27:
+        list_27=["Jamal Murray", "Jack Twyman"]
+        asklegendint27=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint27==1:
+            print(list_27[0])
+        elif asklegendint27==2:
+            print(list_27[1])
+
+    elif inputjersey==28:
+        list_28=["Andre Iguodala", "Andrew Lang"]
+        asklegendint28=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint28==1:
+            print(list_28[0])
+        elif asklegendint28==2:
+            print(list_28[1])
+
+    elif inputjersey==29:
+        list_29=["No Current Player Wears #29", "Paul Silas"]
+        asklegendint29=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint29==1:
+            print(list_29[0])
+        elif asklegendint29==2:
+            print(list_29[1])
+
+    elif inputjersey==30:
+        list_30=["Stephen Curry", "George McGinnis"]
+        asklegendint30=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint30==1:
+            print(list_30[0])
+        elif asklegendint30==2:
+            print(list_30[1])
+
+    elif inputjersey==31:
+        list_31=["Marcus Morris", "Reggie Miller"]
+        asklegendint31=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint31==1:
+            print(list_31[0])
+        elif asklegendint31==2:
+            print(list_31[1])
+
+    elif inputjersey==32:
+        list_32=["Karl-Anthony Towns", "Magic Johnson"]
+        asklegendint32=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint32==1:
+            print(list_32[0])
+        elif asklegendint32==2:
+            print(list_32[1])
+
+    elif inputjersey==33:
+        list_33=["Marc Gasol", "Kareem Abdul-Jabbar"]
+        asklegendint33=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint33==1:
+            print(list_33[0])
+        elif asklegendint33==2:
+            print(list_33[1])
+
+    elif inputjersey==34:
+        list_34=["Giannis Antetokounmpo", "Shaquille O'Neal"]
+        asklegendint34=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint34==1:
+            print(list_34[0])
+        elif asklegendint34==2:
+            print(list_34[1])
+
+    elif inputjersey==35:
+        list_35=["Marvin Bagley III", "Roger Brown"]
+        asklegendint35=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint35==1:
+            print(list_35[0])
+        elif asklegendint35==2:
+            print(list_35[1])
+
+    elif inputjersey==36:
+        list_36=["Marcus Smart", "Rasheed Wallace"]
+        asklegendint36=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint36==1:
+            print(list_36[0])
+        elif asklegendint36==2:
+            print(list_36[1])
+
+    elif inputjersey==37:
+        list_37=["Kostas Antetokounmpo", "Derek Fisher"]
+        asklegendint37=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint37==1:
+            print(list_37[0])
+        elif asklegendint37==2:
+            print(list_37[1])
+
+    elif inputjersey==38:
+        list_38=["No Current Player Wears #38", "Anthony Cook"]
+        asklegendint38=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint38==1:
+            print(list_38[0])
+        elif asklegendint38==2:
+            print(list_38[1])
+
+    elif inputjersey==39:
+        list_39=["Dwight Howard", "Caldwell Jones"]
+        asklegendint39=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint39==1:
+            print(list_39[0])
+        elif asklegendint39==2:
+            print(list_39[1])
+
+    elif inputjersey==40:
+        list_40=["Harrison Barnes", "Shawn Kemp"]
+        asklegendint40=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint40==1:
+            print(list_40[0])
+        elif asklegendint40==2:
+            print(list_40[1])
+
+    elif inputjersey==41:
+        list_41=["Juan Hernangomez", "Dirk Nowitzki"]
+        asklegendint41=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint41==1:
+            print(list_41[0])
+        elif asklegendint41==2:
+            print(list_41[1])
+
+    elif inputjersey==42:
+        list_42=["Al Horford", "Nate Thurmond"]
+        asklegendint42=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint42==1:
+            print(list_42[0])
+        elif asklegendint42==2:
+            print(list_42[1])
+
+    elif inputjersey==43:
+        list_43=["Pascal Siakam", "Jack Sikma"]
+        asklegendint43=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint43==1:
+            print(list_43[0])
+        elif asklegendint43==2:
+            print(list_43[1])
+
+    elif inputjersey==44:
+        list_44=["Bojan Bogdanovic", "Jerry West"]
+        asklegendint44=int(input("Would you like to know a current player, or a retired legend? Press 1 for current player or 2 for retired legend "))
+        if asklegendint44==1:
+            print(list_44[0])
+        elif asklegendint44==2:
+            print(list_44[1])
+
+
+
+
+
+    
+
     
 
 
